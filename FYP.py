30e826bfc8e23df192dcd75b6a5b7007051e4197
HiraMaqbol1997 <61118190+HiraMaqbol1997@users.noreply.github.com>
2020-03-09 09:49:58 +0500

    Gender with Age Prediction

--- FYP.py
+++ FYP.py
@@ -1,78 +1,126 @@
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
-from keras.utils import get_file
-import numpy as np
-import argparse
 import cv2
 import os
-import cvlib as cv
-
-path = get_file("gender_detection.model","/home/hp/PycharmProjects/gender-detection-keras-master", cache_subdir="trained_Model", cache_dir=os.getcwd())
-
-mdl = load_model(path)
-
-camera = cv2.VideoCapture(0)
-
-if not camera.isOpened():
-    print("Error in opening camera")
-    exit()
-    
-categories = ['male','female']
-
-while camera.isOpened():
-
-    check, frame = camera.read()
-
-    if not check:
-        print("Error in Reading Frames")
-        exit()
-
-    face, value = cv.detect_face(frame)
-    print(face)
-    print(value)
+from time import sleep
+import numpy as np
+import argparse
+from Training_Model import WideResNet
 
 
-    for idx, arr in enumerate(face):
-
-        (start_X, start_Y) = arr[0], arr[1]
-        (end_X, end_Y) = arr[2], arr[3]
-
-        cv2.rectangle(frame, (start_X,start_Y), (end_X,end_Y), (200, 255, 200), 3)
-
-        face_rec = np.copy(frame[start_Y:end_Y,start_X:end_X])
-
-        if (face_rec.shape[0]) < 10 or (face_rec.shape[1]) < 10:
-            continue
+from keras.utils.data_utils import get_file
 
 
-        face_rec = cv2.resize(face_rec, (96,96))
-        face_rec = face_rec.astype("float") / 255.0
-        face_rec = img_to_array(face_rec)
-        face_rec = np.expand_dims(face_rec, axis=0)
+class FaceCV(object):
+
+    CASE_PATH = "haarcascade_frontalface_alt.xml"
+    WRN_WEIGHTS_PATH = "https://github.com/Tony607/Keras_age_gender/releases/download/V1.0/weights.18-4.06.hdf5"
 
 
-        conf = mdl.predict(face_rec)[0]
-        print(conf)
-        print(categories)
+    def __new__(cls, weight_file=None, depth=16, width=8, face_size=64):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(FaceCV, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self, depth=16, width=8, face_size=64):
+        self.face_size = face_size
+        self.model = WideResNet(face_size, depth=depth, k=width)()
+        model_dir = os.path.join(os.getcwd(), "pretrained_models").replace("//", "\\")
+        fpath = get_file('weights.18-4.06.hdf5',self.WRN_WEIGHTS_PATH,cache_subdir=model_dir)
+        self.model.load_weights(fpath)
+
+    @classmethod
+    def draw_label(cls, image, point, label, font,font_scale=1, thickness=2):
+        size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+        x, y = point
+        cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
+        cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness)
+
+    def crop_face(self, imgarray, section, margin=40, size=50):
+
+        img_h, img_w, _ = imgarray.shape
+        if section is None:
+            section = [0, 0, img_w, img_h]
+
+        (x, y, w, h) = section
+        margin = int(min(w,h) * margin / 100)
+        x_a = x - margin
+        y_a = y - margin
+        x_b = x + w + margin
+        y_b = y + h + margin
+        if x_a < 0:
+            x_b = min(x_b - x_a, img_w-1)
+            x_a = 0
+
+        if y_a < 0:
+            y_b = min(y_b - y_a, img_h-1)
+            y_a = 0
+
+        if x_b > img_w:
+            x_a = max(x_a - (x_b - img_w), 0)
+            x_b = img_w
+
+        if y_b > img_h:
+            y_a = max(y_a - (y_b - img_h), 0)
+            y_b = img_h
+        cropped = imgarray[y_a: y_b, x_a: x_b]
+        resized_img = cv2.resize(cropped, (size, size), interpolation=cv2.INTER_AREA)
+        resized_img = np.array(resized_img)
+        return resized_img, (x_a, y_a, x_b - x_a, y_b - y_a)
+
+    def detect_face(self):
+        face_cascade = cv2.CascadeClassifier(self.CASE_PATH)
+        video_capture = cv2.VideoCapture(0)
+        while True:
+            if not video_capture.isOpened():
+               sleep(5)
+
+            ret, frame = video_capture.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=10,minSize=(self.face_size, self.face_size))
 
 
-        idx = np.argmax(conf)
-        label = categories[idx]
+            face_imgs = np.empty((len(faces), self.face_size, self.face_size, 3))
+            for i, face in enumerate(faces):
+                face_img, cropped = self.crop_face(frame, face, margin=40, size=self.face_size)
+                (x, y, w, h) = cropped
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 200, 0), 2)
+                face_imgs[i,:,:,:] = face_img
+            if len(face_imgs) > 0:
 
-        label = "{}: {:.2f}%".format(label, conf[idx] * 100)
-
-        Y = start_Y - 10 if start_Y - 10 > 10 else start_Y + 10
-
-
-        cv2.putText(frame, label, (start_X, Y),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (200, 255, 200), 3)
-
-
-    cv2.imshow("gender detection", frame)
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                results = self.model.predict(face_imgs)
+                predicted_genders = results[0]
+                ages = np.arange(0, 101).reshape(101, 1)
+                predicted_ages = results[1].dot(ages).flatten()
 
 
-camera.release()
-cv2.destroyAllWindows()
+            for i, face in enumerate(faces):
+                label = "{}, {}".format(int(predicted_ages[i]-4),"Female" if predicted_genders[i][0] > 0.5 else "Male")
+                self.draw_label(frame, (face[0], face[1]), label, cv2.FONT_HERSHEY_SIMPLEX)
+
+            cv2.imshow('Keras Faces', frame)
+            if cv2.waitKey(50) == 27:  # press ESC
+                break
+
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="This script detects faces from web cam input and estimates age and gender for the detected faces.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--depth", type=int, default=16,help="depth of network")
+    parser.add_argument("--width", type=int, default=8,help="width of network")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    depth = args.depth
+    width = args.width
+
+    face = FaceCV(depth=depth, width=width)
+
+    face.detect_face()
+
+if __name__ == "__main__":
+    main()
+
